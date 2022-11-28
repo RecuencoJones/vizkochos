fe90a57e03b62ac181668f1e2e6933d4beab6b3b
Recuenco, David <david.recuenco@adidas-group.com>
2022-11-28 16:12:09 +0100

    FIX packaging with logo

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Because rendering a list of pods shouldn't be expensive
 
 ## Attributions
 
+- [Bootstrap Icons](https://icons.getbootstrap.com/)
 - [Modified Kubernetes logo](https://github.com/kubernetes/kubernetes/blob/70617042976dc168208a41b8a10caa61f9748617/logo/logo.png)
+- [IconGenerator](https://github.com/onmyway133/IconGenerator)
--- forge.config.js
+++ forge.config.js
@@ -1,24 +1,27 @@
 module.exports = {
+  packagerConfig: {
+    icon: './public/logo'
+  },
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {}
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin']
     },
     {
       name: '@electron-forge/maker-deb',
       config: {
         bin: 'Vizkochos'
-      },
+      }
     },
     {
       name: '@electron-forge/maker-rpm',
       config: {
         bin: 'Vizkochos'
-      },
-    },
-  ],
+      }
+    }
+  ]
 };
